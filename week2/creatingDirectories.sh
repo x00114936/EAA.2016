fb69db362d06989fe4d74f9590a274c392c87ddd
John Burns <jzburns@gmail.com>
2016-10-07 10:13:05 +0100

    first push

---
+++ week2/creatingDirectories.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "This is a script to create directories"
+
+#delete the directory first (to stop errors when script is run again)
+
+rmdir week2
+
+#make the directory
+
+mkdir week2
+
+cd week2
+mkdir subdir1
+mkdir subdir2
+mkdir subdir3
