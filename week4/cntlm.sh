0f677862a38bbcdb0ef9c3093fbb356a7e66c1f2
John Burns <jzburns@gmail.com>
2016-10-14 10:47:35 +0100

    grep

--- week4/cntlm.sh
+++ week4/cntlm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # ps -ef displays info on all the running processes
-#grep mysql searches for the word cntlm as root
-ps -ef | grep cntlm | grep root
+# grep cntlm searches for the word cntlm as root
 
+ps -ef | grep cntlm | grep root
