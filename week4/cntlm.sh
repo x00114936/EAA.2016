0b68795a2ffd6f57502f196b1544232c20b87237
John Burns <jzburns@gmail.com>
2016-10-14 10:33:11 +0100

    grep

---
+++ week4/cntlm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ps -ef displays info on all the running processes
+#grep mysql searches for the word cntlm as root
+ps -ef | grep cntlm | grep root
+
---
+++ week4/pipe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ps -ef displays info on all the running processes
+#grep mysql searches for the word mysql
+ps -ef | grep mysql
+
