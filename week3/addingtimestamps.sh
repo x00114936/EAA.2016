223d155127bba1b68d6c433955ad96e999cddcdd
John Burns <jzburns@gmail.com>
2016-10-19 17:35:05 +0100

    updates to file addingtimestamps.sh

--- week3/addingtimestamps.sh
+++ week3/addingtimestamps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "Are $1 $2 $3 parameters correct?"
+#	$# holds the total num of parameters. $1 $2 $3 are positional parameters. The value held in these is entered when running the script 
+
+echo "$# Parameters enetered. Are $1 $2 $3 parameters correct?"
+
+#this captures the user input into a text file usinf redirect >>
+#this ` runs the command if it is part of an exho statement. In this case, the `date` command that is in the echo statement
+#it will also add the username to the file
+
+echo "$1 $2 $3 $USER `date`" >> $HOME/EAA.2016/week3/audit.txt
 
 
